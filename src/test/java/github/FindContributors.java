ad3adb69f3742bafde29f2e9ef5f77183a037021
Dmitriy <xyzmail@yandex.ru>
2021-06-02 01:04:33 +0400

    Check SoftAssertions

---
+++ src/test/java/github/FindContributors.java
@@ -0,0 +1,18 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+public class FindContributors {
+
+    @Test
+    void firstContributorShouldBeSolntsev(){
+        //открыть страницу selenide
+
+        //подвести мышь к первому элементу
+//        $(".BorderGrid").$(byText("Contributors"))
+//                .closest("div").$("li").hover();
+//
+//        sleep(5000);
+    }
+
+}
